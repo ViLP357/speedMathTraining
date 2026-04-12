cc15a811ceee398f5d0770d9b1d3c7384ad49ce3
ViLP357 <vilipi2009@gmail.com>
2026-04-12 19:20:31 +0300

    new files

---
+++ multiplier.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const multiplicator = (a, b, printText) => {
+    console.log(printText, a * b);
+};
+multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is:');
