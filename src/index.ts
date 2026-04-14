2632faa7cd2016827bd1fbd1dee285cdae282676
ViLP357 <vilipi2009@gmail.com>
2026-04-14 20:35:12 +0300

    finally working project base

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express'
+import { Request, Response, NextFunction } from 'express'
+
+const app = express()
+const port = 3000
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Welcome to train ypur mental math skills!")
+})
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(500).json({
+        status: 500,
+        message: "There was an error",
+        error: err.message
+    })
+})
+
+app.listen(port, () => {
+    console.log(`Server running at http//localhost:${port}`)
+})
